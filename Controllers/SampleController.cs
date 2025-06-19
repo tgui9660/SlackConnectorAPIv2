4363a535490463f92ab68b0c23ddd9c67556dcb2
eric <eric@ecmorgan.net>
2025-06-19 14:15:58 -0400

    Added more functionality

--- Controllers/SampleController.cs
+++ Controllers/SampleController.cs
@@ -4,10 +4,114 @@ using SlackNet.WebApi;
 
 namespace SlackConnectorAPIv2.Controllers;
 
+// Good tutorial on setting up SlackNet and Slack API configurations
+// You'll need your oath token!
+// -- -- looks like: xoxp-989876-86896976-945595859876789-987sadf67sd5f56s87sa5fdsd1b
+// -- https://danielko.medium.com/building-an-app-as-an-agent-for-slack-with-net-959e1c2ba3af
+//
+// SlackNet .Net library
+// -- https://github.com/soxtoby/SlackNet 
+//
+// Above library implements these slack calls
+// -- https://api.slack.com/methods
+
+
 [ApiController]
 [Route("[controller]")]
 public class SampleController : ControllerBase
 {
+    // Send message to channel
+    [HttpGet("SendChannelMessage")]
+    public async Task<string> SendChannelMessage(string oathToken, string channel, string message)
+    {
+
+        try
+        {
+            // SlackNet send message to channel
+            var api = new SlackServiceBuilder().UseApiToken(oathToken).GetApiClient();
+
+            // Send a message
+            await api.Chat.PostMessage(new Message { Text = "Bot Message: [" + message + "]", Channel = channel });
+
+            return "Everything executed OK!";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e.Message;
+        }
+    }
+
+    // Send message to channel
+    [HttpGet("GetAvailableRealNames")]
+    public async Task<List<string>> GetAvailableRealNames(string oathToken)
+    {
+
+        List<string> userRealNamesFound = new List<string>();
+
+        var api = new SlackServiceBuilder().UseApiToken(oathToken).GetApiClient();
+
+        UserListResponse listOfUsers = await api.Users.List();
+
+        foreach (User theUser in listOfUsers.Members)
+        {
+            if (theUser.RealName != null)
+            {
+                userRealNamesFound.Add(theUser.RealName);
+            }
+        }
+        return userRealNamesFound;
+    }
+
+    // Send message to channel
+    [HttpGet("SendMessageToRealName")]
+    public async Task<string> SendMessageToRealName(string oathToken, string humanName, string message)
+    {
+        string userIDFromHumanName = "";
+        string userRealNamesFound = "";
+        string userNames = "";
+
+        string returnPayload = "";
+
+        try
+        {
+            var api = new SlackServiceBuilder().UseApiToken(oathToken).GetApiClient();
+
+            UserListResponse listOfUsers = await api.Users.List();
+
+            foreach (User theUser in listOfUsers.Members)
+            {
+                if (theUser.RealName != null)
+                {
+                    userRealNamesFound = userRealNamesFound + " : " + theUser.RealName.ToLower();
+                }
+
+                if (theUser.Name != null)
+                {
+                    userNames = userNames + " : " + theUser.Name.ToLower();
+                }
+
+                if (theUser.RealName != null && theUser.RealName.ToLower() == humanName.Trim().ToLower())
+                {
+                    userIDFromHumanName = theUser.Id;
+                    // break;
+                }
+            }
+            returnPayload = "humanName: " + humanName + " userIDFromHumanName: " + userIDFromHumanName + "  message: " + message + " userRealNamesFound: [" + userRealNamesFound + "] " + " userNames: [" + userNames + "] ";
+            Message theMessage = new Message();
+            theMessage.Channel = userIDFromHumanName;
+            theMessage.Text = message;
+
+            PostMessageResponse messageResponse = await api.Chat.PostMessage(theMessage);
+
+            return "Execution OK!!: " + returnPayload;
+        }
+        catch (Exception e)
+        {
+            return returnPayload + " Error: " + e.Message + " : " + e.StackTrace;
+        }
+    }
+
+
     // Test Method for sanity
     [HttpGet("echo")]
     public string echo(string testMessage)
@@ -21,23 +125,4 @@ public class SampleController : ControllerBase
     {
         return "pong!";
     }
-
-    // Send message to channel
-    [HttpGet("SendTestSlackMessage")]
-    public async void SendTestSlackMessage(string oathToken, string channel, string testMessage)
-    {
-        // Good tutorial on setting up SlackNet and Slack API configurations
-        // You'll need your oath token!
-        // -- -- looks like: xoxp-989876-86896976-945595859876789-987sadf67sd5f56s87sa5fdsd1b
-        // -- https://danielko.medium.com/building-an-app-as-an-agent-for-slack-with-net-959e1c2ba3af
-        //
-        // SlackNet .Net library
-        // -- https://github.com/soxtoby/SlackNet 
-
-        // SlackNet send message to channel
-        var api = new SlackServiceBuilder().UseApiToken(oathToken).GetApiClient();
-
-        // Send a message
-        await api.Chat.PostMessage(new Message { Text = "Test Message: " + testMessage, Channel = channel });
-    } 
 }
